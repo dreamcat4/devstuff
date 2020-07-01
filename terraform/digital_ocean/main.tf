8fa86912fa4cbd3700f53da91e5aff8205693cd3
habbis <habbis@medmail.ch>
2020-07-01 07:58:51 +0200

    added terraform

---
+++ terraform/digital_ocean/main.tf
@@ -0,0 +1,19 @@
+variable "do_token" {}
+
+
+data "digitalocean_ssh_key" "example" {
+  name = "example"
+}
+
+resource "digitalocean_vpc" "example" {
+  name     = "example-project-network"
+  region   = "fra1"
+}
+
+resource "digitalocean_droplet" "example" {
+  name     = "example-01"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-18-04-x64"
+  region   = "fra1"
+  vpc_uuid = digitalocean_vpc.example.id
+}
