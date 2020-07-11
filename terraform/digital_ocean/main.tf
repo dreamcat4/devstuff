a6d524b435ef4c8d9eb16666d5bc1b5935ddad2d
habbis <habbis@medmail.ch>
2020-07-11 14:42:43 +0200

    11-07-2020_14-42-43

--- terraform/digital_ocean/main.tf
+++ terraform/digital_ocean/main.tf
@@ -6,28 +6,34 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# my ssh keys in my digital ocean
-data "digitalocean_ssh_key" "latitude7490" {
+variable "cidr_subnet" {
+  description = "CIDR block for the internal fra1 subnet"
+  default = "10.2.1.0/24"
+}
+
+# my ssh keys in my digital ocean account change when using 
+# own account
+data "digitalocean_ssh_key" "ssh_keys01" {
   name = "latitude7490"
 }
 
 
-data "digitalocean_ssh_key" "ebbestad_linbast" {
+data "digitalocean_ssh_key" "ssh_keys02" {
   name = "ebbestad_linbast"
 }
 
 
-data "digitalocean_ssh_key" "yubikey_rundthalsen3" {
+data "digitalocean_ssh_key" "ssh_keys03" {
   name = "yubikey_rundthalsen3"
 }
 
 
-data "digitalocean_ssh_key" "yubikey_laptop3" {
+data "digitalocean_ssh_key" "ssh_keys04" {
   name = "yubikey_laptop3"
 }
 
 
-data "digitalocean_ssh_key" "ryzen5" {
+data "digitalocean_ssh_key" "ssh_keys05" {
   name = "ryzen5"
 }
 
@@ -43,7 +49,7 @@ data "digitalocean_ssh_key" "ryzen5" {
 resource "digitalocean_vpc" "fra2-net" {
   name     = "fra2-net"
   region   = "fra1"
-  ip_range = "10.2.1.0/24"
+  ip_range = "${var.cidr_subnet}"
 }
 
 # create volume for storage
@@ -66,11 +72,36 @@ resource "digitalocean_droplet" "cloudbast01" {
   image    = "ubuntu-18-04-x64"
   region   = "fra1"
   vpc_uuid = digitalocean_vpc.fra2-net.id
-  ssh_keys = [data.digitalocean_ssh_key.latitude7490.id, data.digitalocean_ssh_key.ryzen5.id, data.digitalocean_ssh_key.ebbestad_linbast.id, data.digitalocean_ssh_key.yubikey_rundthalsen3.id, data.digitalocean_ssh_key.yubikey_laptop3.id ]
+  ssh_keys = [data.digitalocean_ssh_key.ssh_keys01.id, data.digitalocean_ssh_key.ssh_keys02.id, data.digitalocean_ssh_key.ssh_keys03.id, data.digitalocean_ssh_key.ssh_keys04.id, data.digitalocean_ssh_key.ssh_keys05.id ]
   #ip_address = digitalocean_droplet.test01.ipv4_address
 
 }
 
+
+# create server
+resource "digitalocean_droplet" "mariadb01" {
+  name     = "mariadb01"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-18-04-x64"
+  region   = "fra1"
+  vpc_uuid = digitalocean_vpc.fra2-net.id
+  ssh_keys = [data.digitalocean_ssh_key.ssh_keys01.id, data.digitalocean_ssh_key.ssh_keys02.id, data.digitalocean_ssh_key.ssh_keys03.id, data.digitalocean_ssh_key.ssh_keys04.id, data.digitalocean_ssh_key.ssh_keys05.id ]
+  #ip_address = digitalocean_droplet.test01.ipv4_address
+
+}
+
+
+# create reverse proxy server
+resource "digitalocean_droplet" "proxy01" {
+  name     = "proxy01"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-18-04-x64"
+  region   = "fra1"
+  vpc_uuid = digitalocean_vpc.fra2-net.id
+  ssh_keys = [data.digitalocean_ssh_key.ssh_keys01.id, data.digitalocean_ssh_key.ssh_keys02.id, data.digitalocean_ssh_key.ssh_keys03.id, data.digitalocean_ssh_key.ssh_keys04.id, data.digitalocean_ssh_key.ssh_keys05.id ]
+  #ip_address = digitalocean_droplet.test01.ipv4_address
+
+}
 # create server
 resource "digitalocean_droplet" "media01" {
   name     = "media01"
@@ -78,7 +109,7 @@ resource "digitalocean_droplet" "media01" {
   image    = "ubuntu-18-04-x64"
   region   = "fra1"
   vpc_uuid = digitalocean_vpc.fra2-net.id
-  ssh_keys = [data.digitalocean_ssh_key.latitude7490.id, data.digitalocean_ssh_key.ryzen5.id, data.digitalocean_ssh_key.ebbestad_linbast.id, data.digitalocean_ssh_key.yubikey_rundthalsen3.id, data.digitalocean_ssh_key.yubikey_laptop3.id ]
+  ssh_keys = [data.digitalocean_ssh_key.ssh_keys01.id, data.digitalocean_ssh_key.ssh_keys02.id, data.digitalocean_ssh_key.ssh_keys03.id, data.digitalocean_ssh_key.ssh_keys04.id, data.digitalocean_ssh_key.ssh_keys05.id ]
   #ip_address = digitalocean_droplet.test01.ipv4_address
 
 }
@@ -90,7 +121,7 @@ resource "digitalocean_droplet" "media02" {
   image    = "ubuntu-18-04-x64"
   region   = "fra1"
   vpc_uuid = digitalocean_vpc.fra2-net.id
-  ssh_keys = [data.digitalocean_ssh_key.latitude7490.id, data.digitalocean_ssh_key.ryzen5.id, data.digitalocean_ssh_key.ebbestad_linbast.id, data.digitalocean_ssh_key.yubikey_rundthalsen3.id, data.digitalocean_ssh_key.yubikey_laptop3.id ]
+  ssh_keys = [data.digitalocean_ssh_key.ssh_keys01.id, data.digitalocean_ssh_key.ssh_keys02.id, data.digitalocean_ssh_key.ssh_keys03.id, data.digitalocean_ssh_key.ssh_keys04.id, data.digitalocean_ssh_key.ssh_keys05.id ]
   #ip_address = digitalocean_droplet.test01.ipv4_address
 
 }
@@ -108,38 +139,29 @@ resource "digitalocean_firewall" "cloudbast" {
     source_addresses = ["0.0.0.0/0"]
 }
 
-# allow out from ubuntu source.list
-# digitaloceanmirrors.com,security.ubuntu.com, archive.ubuntu.com
-
-  outbound_rule {
-    protocol         = "tcp"
-    port_range       = "all"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
-  }
-
   outbound_rule {
     protocol         = "tcp"
     port_range       = "443"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
   outbound_rule {
     protocol         = "tcp"
     port_range       = "80"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
-}
+    destination_addresses = ["0.0.0.0/0"]
+  }
 
   outbound_rule {
     protocol         = "tcp"
     port_range       = "53"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
 
   outbound_rule {
     protocol         = "udp"
     port_range       = "53"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
- }
+}
 
 # firewall rules with more than one droplet defined
 resource "digitalocean_firewall" "media" {
@@ -153,29 +175,134 @@ resource "digitalocean_firewall" "media" {
     source_addresses = ["10.2.1.0/24"]
 }
 
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["10.2.1.0/24"]
+}
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["10.2.1.0/24"]
+}
+
 # allow out from ubuntu source.list
 # digitaloceanmirrors.com,security.ubuntu.com, archive.ubuntu.com
 
   outbound_rule {
     protocol         = "tcp"
     port_range       = "443"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
   outbound_rule {
     protocol         = "tcp"
     port_range       = "80"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
 
   outbound_rule {
     protocol         = "tcp"
     port_range       = "53"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
 
   outbound_rule {
     protocol         = "udp"
     port_range       = "53"
-    destination_addresses = ["104.24.116.209/32", "104.24.117.209/32", "172.67.168.253/32", "91.189.88.142/32", "91.189.88.152/32", "91.189.91.38/32", "91.189.91.39/32", "91.189.88.142/32", "91.189.88.152/32"]
+    destination_addresses = ["0.0.0.0/0"]
   }
 }
+
+# firewall rules with more than one droplet defined
+resource "digitalocean_firewall" "database" {
+  name = "app-allow-out-HTTPS-HTTP-in-SSH-MYSQL-from-fra2"
+
+  droplet_ids = [digitalocean_droplet.mariadb01.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["10.2.1.0/24"]
+}
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "3306"
+    source_addresses = ["10.2.1.0/24"]
+}
+
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "53"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol         = "udp"
+    port_range       = "53"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+}
+
+
+# firewall rules with more than one droplet defined
+resource "digitalocean_firewall" "proxy" {
+  name = "app-allow-out-HTTPS-HTTP-in-SSH-from-fra2-allow-in-HTTP-HTTPS"
+
+  droplet_ids = [digitalocean_droplet.proxy01.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["10.2.1.0/24"]
+}
+
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["10.2.1.0/24"]
+}
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["10.2.1.0/24"]
+}
+
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    destination_addresses = ["0.0.0.0/0"]
+}
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    destination_addresses = ["0.0.0.0/0"]
+}
+
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "53"
+    destination_addresses = ["0.0.0.0/0"]
+}
+
+  outbound_rule {
+    protocol         = "udp"
+    port_range       = "53"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+}
+
